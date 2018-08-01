fc6e7cd60b7092e810472a232a39183fb9523eab
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-08-01 22:40:13 +0200

    fix example site build

---
+++ examples/webpack.common.js
@@ -0,0 +1,31 @@
+const path = require ('path');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const htmlWebpackPlugin = new HtmlWebpackPlugin ({
+  template: path.join (__dirname, 'src/index.html'),
+  filename: './index.html',
+});
+
+module.exports = {
+  entry: {app: path.join (__dirname, 'src/index.js')},
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve (__dirname, 'dist'),
+  },
+};
